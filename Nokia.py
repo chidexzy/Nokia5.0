7f99057c5dbc1619abf3c86b57863bdfd7c3a489
chidexzy <66396748+chidexzy@users.noreply.github.com>
2024-07-11 18:28:45 +0100

    Create Nokia.py

---
+++ Nokia.py
@@ -0,0 +1,46 @@
+import os, sys, platform, time
+from time import sleep
+try:
+    import requests
+except:
+    os.system('pip install requests')
+def brand():
+	os.system('clear')
+	print("""
+██████╗ ██████╗ ███████╗██╗  ██╗
+██╔══██╗██╔══██╗██╔════╝╚██╗██╔╝
+██║  ██║██████╔╝█████╗   ╚███╔╝ 
+██║  ██║██╔══██╗██╔══╝   ██╔██╗ 
+██████╔╝██║  ██║███████╗██╔╝ ██╗
+╚═════╝ ╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝
+                                """)
+	time.sleep(1)
+	os.system('clear')
+	print("""
+██╗███████╗
+██║██╔════╝
+██║███████╗
+██║╚════██║
+██║███████║
+╚═╝╚══════╝
+           """)
+	time.sleep(1)
+	os.system('clear')
+	print("""
+██╗  ██╗██╗███╗   ██╗ ██████╗ 
+██║ ██╔╝██║████╗  ██║██╔════╝ 
+█████╔╝ ██║██╔██╗ ██║██║  ███╗
+██╔═██╗ ██║██║╚██╗██║██║   ██║
+██║  ██╗██║██║ ╚████║╚██████╔╝
+╚═╝  ╚═╝╚═╝╚═╝  ╚═══╝ ╚═════╝ 
+                              """)
+	time.sleep(1)
+	os.system('clear')
+brand()
+bit = platform.architecture()[0]
+if bit == '64bit':
+    import nok
+elif bit == '32bit':
+    import nok
+  
+￼Enter
